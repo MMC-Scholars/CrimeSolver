62179f05a07dffc878906ccb69716086b8fe8945
William Delo <55678535+wdelo@users.noreply.github.com>
2020-09-10 15:29:34 -0400

    Had an extra CrimeSolver folder inside the repo folder, moved everything into main folder. - Will

---
+++ Source/CrimeSolver/CrimeSolverGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CrimeSolverGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRIMESOLVER_API ACrimeSolverGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
