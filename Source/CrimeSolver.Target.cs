e5bac3568c2606d1b368ff91095134710bdd260f
William Delo <55678535+wdelo@users.noreply.github.com>
2020-09-10 15:44:09 -0400

    Updated CrimeSolver.Build.cs to include develop version of VRBase. -Will.

--- Source/CrimeSolver.Target.cs
+++ Source/CrimeSolver.Target.cs
@@ -5,10 +5,10 @@ using System.Collections.Generic;
 
 public class CrimeSolverTarget : TargetRules
 {
-	public CrimeSolverTarget( TargetInfo Target) : base(Target)
+	public CrimeSolverTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "CrimeSolver" } );
+		ExtraModuleNames.Add("CrimeSolver");
 	}
 }
--- Source/CrimeSolver/CrimeSolver.Build.cs
+++ Source/CrimeSolver/CrimeSolver.Build.cs
@@ -1,23 +1,17 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
+using System.IO;
 using UnrealBuildTool;
 
-public class CrimeSolver : ModuleRules
-{
-	public CrimeSolver(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+public class CrimeSolver : ModuleRules {
+    public CrimeSolver(ReadOnlyTargetRules Target) : base(Target) {
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore", "ProceduralMeshComponent" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        PublicIncludePaths.AddRange(
+            new string[]
+            {
+                Path.GetFullPath(Path.Combine(ModuleDirectory, "VRBase")),
+                Path.GetFullPath(ModuleDirectory)
+            }
+        );
+    }
 }
