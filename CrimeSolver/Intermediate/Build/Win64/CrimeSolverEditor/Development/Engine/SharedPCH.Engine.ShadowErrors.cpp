b97089e3bad0ffa12d1306451b73b9e7f9a233d9
William Delo <55678535+wdelo@users.noreply.github.com>
2020-09-10 15:25:52 -0400

    Created blank project and added VRBase - Will

---
+++ CrimeSolver/Intermediate/Build/Win64/CrimeSolverEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/William/source/repos/CrimeSolver/CrimeSolver/Intermediate/Build/Win64/CrimeSolverEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ CrimeSolver/Intermediate/Build/Win64/UE4Editor/Inc/CrimeSolver/CrimeSolverGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CRIMESOLVER_CrimeSolverGameModeBase_generated_h
+#error "CrimeSolverGameModeBase.generated.h already included, missing '#pragma once' in CrimeSolverGameModeBase.h"
+#endif
+#define CRIMESOLVER_CrimeSolverGameModeBase_generated_h
+
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_SPARSE_DATA
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_RPC_WRAPPERS
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACrimeSolverGameModeBase(); \
+	friend struct Z_Construct_UClass_ACrimeSolverGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACrimeSolverGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CrimeSolver"), NO_API) \
+	DECLARE_SERIALIZER(ACrimeSolverGameModeBase)
+
+
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACrimeSolverGameModeBase(); \
+	friend struct Z_Construct_UClass_ACrimeSolverGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACrimeSolverGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CrimeSolver"), NO_API) \
+	DECLARE_SERIALIZER(ACrimeSolverGameModeBase)
+
+
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrimeSolverGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrimeSolverGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrimeSolverGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrimeSolverGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrimeSolverGameModeBase(ACrimeSolverGameModeBase&&); \
+	NO_API ACrimeSolverGameModeBase(const ACrimeSolverGameModeBase&); \
+public:
+
+
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrimeSolverGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrimeSolverGameModeBase(ACrimeSolverGameModeBase&&); \
+	NO_API ACrimeSolverGameModeBase(const ACrimeSolverGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrimeSolverGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrimeSolverGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrimeSolverGameModeBase)
+
+
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_12_PROLOG
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_SPARSE_DATA \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_RPC_WRAPPERS \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_INCLASS \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_SPARSE_DATA \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CRIMESOLVER_API UClass* StaticClass<class ACrimeSolverGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CrimeSolver_Source_CrimeSolver_CrimeSolverGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
